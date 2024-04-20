20558b85eeadc14c8b86e6b21a7ee946d753d68d
Sam Damazio <128763505+samdamazio@users.noreply.github.com>
2024-04-20 18:34:20 +0000

    refat

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ app.use(express.json());
 app.post('/split', (req, res) => {
     const { sentence } = req.body;
     if (typeof sentence !== 'string') {
-        return res.status(400).json({ error: 'Input invalido, voce deve enviar uma string.' });
+        return res.status(400).json({ error: 'Invalid input, please provide a string.' });
     }
-    // Usando uma expressão regular para splitar por "." ou " e "
-    const splitSentence = sentence.split(/\.|\s+e\s+/);
+    const splitSentence = sentence.split('.');
     res.json({ splitSentence });
 });
 
