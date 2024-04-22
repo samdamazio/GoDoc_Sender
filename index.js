8b9292496af26adae4f079b7dea4707d53285e54
Sam Damazio <128763505+samdamazio@users.noreply.github.com>
2024-04-22 23:04:20 +0000

    tirando splits de links

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/split', (req, res) => {
-    const { sentence } = req.body;
-    if (typeof sentence !== 'string') {
-        return res.status(400).json({ error: 'Invalid input, please provide a string.' });
-    }
-    const splitSentence = sentence.split('.');
-    res.json({ splitSentence });
+app.post("/split", (req, res) => {
+  const { sentence } = req.body;
+  if (typeof sentence !== "string") {
+    return res
+      .status(400)
+      .json({ error: "Invalid input, please provide a string." });
+  }
+  const splitSentence = sentence.split(". ");
+  res.json({ splitSentence });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
